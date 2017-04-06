62f1ab8b54cf47332ba665bcc37362721e603726
Warren Buckley <wbuckley15@hotmail.com>
2017-04-06 08:42:26 -0400

    big commit

--- app.js
+++ app.js
@@ -1,21 +1,43 @@
 import express from 'express';
 import chalk from 'chalk';
+import Service from './services/svc';
+import Configuration from './services/configuration';
+import portScanner from 'portscanner';
 
-const port = process.env.PORT || 9000;
+const config = new Configuration();
+
+
+
+// Determine if the port supplied by the configuration
+// is already in use.
+portScanner.checkPortStatus(config.port, '127.0.0.1').then( result => {
+
+}).catch(err => {
+
+});
+
+
+
 
 const app = express();
+const port = config.port;
 
-app.get( '/', ( req, res ) =>
-{
-    let serviceStatus = {
-        state: 'Running',
-        name: 'Rip Agent'
-    };
 
-    res.send( serviceStatus );
-} );
+let service = new Service(config.serviceName);
 
-app.listen( port, () =>
-{
-    console.log( chalk.green( `Listening on port ${port}` ) );
-} );
+const router = express.Router();
+router.route('/state')
+    .get((req, res) => {
+        service.serviceStatus().then(result => {
+            res.json(result);
+        }).catch(err => {
+            res.status(400).send(`Cannot find service [${service.name}]`);
+        });
+    });
+app.use('/', router);
+
+app.listen(port, () => {
+    console.log(chalk.green(`Listening on port ${port}`));
+    console.log(chalk.red(__dirname));
+});
+
---
+++ services/logging.js
@@ -0,0 +1,3 @@
+/**
+ * Created by wbuck on 2017-04-06.
+ */
